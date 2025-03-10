f6a3aa043526351d15af740018dbbf86ec20ead7
Saikiruthiga <saikiruthiga06@gmail.com>
2025-03-10 15:06:28 +0100

    use middleware cors for all routes

--- quiz-app/api/server.js
+++ quiz-app/api/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import quizRoutes from "./routes/quizRoutes.js";
 
 const app = express(); // express application instance
 const PORT = 5000;
+app.use(cors());
 
 app.use(express.json()); //middleware to parse JSON - convert incoming JSON request into javascript objects. without this Express app wont understand JSON request bodies
 app.use("/api", quizRoutes);
